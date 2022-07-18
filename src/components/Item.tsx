dda243582b4a91003b74812640949b42cd37af4d
Kylie <kylie@comets.kr>
2022-07-18 11:07:38 +0900

    feat: change numberformat

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -7,12 +7,15 @@ type Props = {
   item: IShopItem;
 };
 export const Item: FC<Props> = ({ item }) => {
+  const numberFormat = (price: number): string => {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
   return (
     <Wrapper>
       <img src={item.image} alt={item.title} />
       <div>
         <h3>{item.title}</h3>
-        <h3>${item.price}</h3>
+        <h3>{numberFormat(item.price)}원</h3>
       </div>
       <Button>Add To Cart</Button>
     </Wrapper>
